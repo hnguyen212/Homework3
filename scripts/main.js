6ef993973550001764012dcdd538c1491f58fce4
hnguyen212 <hnguyen212@csu.fullerton.edu>
2019-09-22 18:44:26 -0700

    Finished Chapter 11 & added payment

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,16 @@
 (function(window) {
-  'use strict';
-  var FORM_SELECTOR = '[data-coffee-order="form"]';
-  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+  "use strict";
+  var FORM_SELECTOR = "[data-coffee-order=\"form\"]";
+  var CHECKLIST_SELECTOR = "[data-coffee-order=\"checklist\"]";
 
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  var Validation = App.Validation;
   var CheckList = App.CheckList;
 
-  var myTruck = new Truck('ncc-1701', new DataStore());
+  var myTruck = new Truck("ncc-1701", new DataStore());
   window.myTruck = myTruck;
 
   var checkList = new CheckList(CHECKLIST_SELECTOR);
@@ -21,4 +22,6 @@
     myTruck.createOrder.call(myTruck, data);
     checkList.addRow.call(checkList, data);
   });
+
+  formHandler.addInputHandler(Validation.isCompanyEmail);
 })(window);
